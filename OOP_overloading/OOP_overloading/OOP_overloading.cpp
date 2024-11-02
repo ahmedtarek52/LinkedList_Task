0b6f3ac37869afe95c0804f37f5193b7d9ec32e3
ahmedtarek52 <a7mdtarek52@gmail.com>
2024-11-02 18:30:02 +0200

    Add Bonus Task

--- OOP_overloading/OOP_overloading/OOP_overloading.cpp
+++ OOP_overloading/OOP_overloading/OOP_overloading.cpp
@@ -1,79 +1,65 @@
 #include <iostream>
-
-
 using namespace std;
 
 class vector3 {
 public:
     float x, y, z;
 
-    
-    //vector3() {};
-    vector3(float xVal=0, float yval=0, float zVal=0) {
-        x = xVal;
-        y = yval;
-        z = zVal;
+    vector3(float xVal = 0, float yVal = 0, float zVal = 0) : x(xVal), y(yVal), z(zVal) {}
+
+    vector3(const vector3& original) : x(original.x), y(original.y), z(original.z) {}
+
+
+    vector3& operator++() {
+        ++x;
+        ++y;
+        ++z;
+        return *this;
     }
 
-    vector3(const vector3& original) {
-        x = original.x;
-        y = original.y;
-        z = original.z;
-    }
-
- 
-    bool operator==( vector3& right) {
-        return (x == right.x && y == right.y && z == right.z);
-        
-        //if(this->x ==right.x&& this->x == right.x&& this->x == right.x){
-        //}
-        //
-    }
-
-
-    bool operator!=(const vector3& right) const {
-        return (x != right.x || y != right.y || z != right.z);
-
-    }
-
-    
-  
-    vector3 operator +(const vector3& right) {
-        vector3 result ;
-        result.x = this->x + right.x;
-        result.y = this->y + right.y;
-        result.z = this->z + right.z;
-        //cout << "plus operator";
-        return result;
-    }
-
-    vector3 operator -(const vector3& right) {
-        vector3 result;
-        result.x = this->x - right.x;
-        result.y = this->y - right.y;
-        result.z = this->z - right.z;
-        //cout << "Subtraction operator";
-        return result;
-
-    }
    
+    vector3 operator++(int) {
+        vector3 temp(*this); 
+        x++;
+        y++;
+        z++;
+        return temp; 
+    }
+
+
+    bool operator==(const vector3& right) const {
+        return (x == right.x && y == right.y && z == right.z);
+    }
 
     
-    void  operator=(const vector3& right) {
-      
+    bool operator!=(const vector3& right) const {
+        return !(*this == right);
+    }
+
+    
+    vector3 operator+(const vector3& right) const {
+        return vector3(x + right.x, y + right.y, z + right.z);
+    }
+
+    vector3 operator-(const vector3& right) const {
+        return vector3(x - right.x, y - right.y, z - right.z);
+    }
+
+    
+    vector3& operator=(const vector3& right) {
+        if (this != &right) { 
             x = right.x;
             y = right.y;
             z = right.z;
-        
-        
+        }
+        return *this;
     }
 
-    
-    explicit operator float()  {
-        return x ;
+    explicit operator float() const {
+        return x;
     }
 
-    
+
     void print() const {
         cout << "(" << x << ", " << y << ", " << z << ")" << endl;
     }
@@ -83,14 +69,27 @@ int main() {
     vector3 vec1(1, 2, 3);
     vector3 vec2(4, 5, 6);
 
+    cout << "Initial vec1: ";
+    vec1.print();
 
 
-    
+    cout << "Using post-increment vec1++: ";
+    vector3 tempVec1 = vec1++;
+    tempVec1.print(); 
+
+    cout << "After post-increment, vec1: ";
+    vec1.print(); 
+
+
+    cout << "Using pre-increment ++vec1: ";
+    ++vec1;
+    vec1.print(); // Should print (3, 4, 5)
+
+
     cout << "vec1 == vec2: " << (vec1 == vec2 ? "true" : "false") << endl;
     cout << "vec1 != vec2: " << (vec1 != vec2 ? "true" : "false") << endl;
-    
 
-    
+
     vector3 resultAdd = vec1 + vec2;
     cout << "Addition result: ";
     resultAdd.print();
@@ -99,12 +98,13 @@ int main() {
     cout << "Subtraction result: ";
     resultSub.print();
 
+
     vec1 = vec2;
-    cout << "Equal result: " << vec1.x << '\n';
+    cout << "After assignment (vec1 = vec2), vec1: ";
+    vec1.print();
 
 
-   
-    cout << (float)resultAdd;
+    cout << "Casting as float: " << static_cast<float>(vec1) << endl;
 
     return 0;
 }
